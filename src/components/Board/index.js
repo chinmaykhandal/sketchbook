bdad37906141ae6fd69be1027fc8304d4c05bf8b
Chinmay Khandal <khandalchinmay@gmail.com>
2024-01-15 21:32:53 +0530

    socket changes done

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,11 +3,13 @@ import { actionItemClick } from "@/slice/menuSlice";
 import React, { useEffect, useLayoutEffect } from "react";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { socket } from "@/socket";
 const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
+  const drawHistory = useRef([])
+  const historyPointer = useRef(0)
   const {activeMenuItem, actionMenuItem}  = useSelector((state) => state.menu);
   const {color, size} = useSelector((state) => state.toolbox[activeMenuItem]);
 
@@ -22,23 +24,38 @@ const Board = () => {
       anchor.href = URL
       anchor.download = 'sketch.jpg'
       anchor.click()
-    }
+    }else  if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
+      if(historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1
+      if(historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1
+      const imageData = drawHistory.current[historyPointer.current]
+      context.putImageData(imageData, 0, 0)
+  }
     dispatch(actionItemClick(null));
     console.log("Action Menu Item", actionMenuItem)
   }, [actionMenuItem])
 
   useEffect(() => {
-    if (!canvasRef.current) return;
+    if (!canvasRef.current) return
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+    const context = canvas.getContext('2d')
 
-    const changeConfig = () => {
-      context.strokeStyle = color;
-      context.lineWidth = size;
-    };
+    const changeConfig = (color, size) => {
+        context.strokeStyle = color
+        context.lineWidth = size
+    }
 
-    changeConfig();
-  }, [color, size]);
+    const handleChangeConfig = (config) => {
+        console.log("config", config)
+        changeConfig(config.color, config.size)
+    }
+    changeConfig(color, size)
+    socket.on('changeConfig', handleChangeConfig)
+
+    return () => {
+        socket.off('changeConfig', handleChangeConfig)
+    }
+}, [color, size])
+
 
   useLayoutEffect(() => {
     if (!canvasRef.current) return;
@@ -63,25 +80,46 @@ const Board = () => {
       shouldDraw.current = true;
       context.beginPath();
       beginPath(e.clientX, e.clientY)
+      socket.emit("beginPath",{x: e.clientX, y: e.clientY})
     };
 
     const handleMouseMove = (e) => {
       if (!shouldDraw.current === true) return;
       drawLine(e.clientX, e.clientY)
+      socket.emit("drawLine",{x: e.clientX, y: e.clientY})
+
     };
 
     const handleMouseUp = (e) => {
       shouldDraw.current = false;
+      const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length - 1;
+
     };
+
+    const handleBeginPath = (path) => {
+      beginPath(path.x, path.y)
+    }
+
+    const handleDrawLine = (path) => {
+      drawLine(path.x, path.y)
+    }
 
     canvas.addEventListener("mousedown", handleMouseDown);
     canvas.addEventListener("mousemove", handleMouseMove);
     canvas.addEventListener("mouseup", handleMouseUp);
 
+    socket.on('beginPath', handleBeginPath);
+    socket.on('drawLine', handleDrawLine);
+
     return () => {
       canvas.removeEventListener("mousedown", handleMouseDown);
       canvas.removeEventListener("mousemove", handleMouseMove);
       canvas.removeEventListener("mouseup", handleMouseUp);
+
+      socket.off('beginPath', handleBeginPath);
+      socket.off('drawLine', handleDrawLine);
     };
   }, []);
 
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -4,6 +4,7 @@ import { COLORS, MENU_ITEMS } from '@/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import {changeColor, changeBrushSize} from "@/slice/toolboxSlice";
 import cx from "classnames"
+import { socket } from "@/socket";
 
 const Toolbox = () => {
     const dispatch = useDispatch();
@@ -14,10 +15,13 @@ const Toolbox = () => {
 
     const updateBrushSize = (e) => {
         dispatch(changeBrushSize({item: activeMenuItem, size: e.target.value}))
+        socket.emit('changeConfig', {color, size: e.target.value});
     }
 
     const updateColor = (newColor) => {
         dispatch(changeColor({item: activeMenuItem, color: newColor}))
+        socket.emit('changeConfig', {color: newColor, size});
+
     }
   return (
     <div className={styles.toolboxContainer}>
